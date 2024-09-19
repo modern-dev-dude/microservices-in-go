e81c688204113f2cc72d7aac8ff9dc7f3afcc7d1
Jason Stoddard <jason.stoddard@crowdstrike.com>
2024-09-19 08:16:13 -0400

    added some of the modules in chapter 2

---
+++ projects/cmd/cp2/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json:"fullName" xml:"fullName"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zipCode" xml:"zipCode"`
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello")
+}
+
+func main() {
+	http.HandleFunc("/greet", greetHandler)
+	http.HandleFunc("/customers", getAllCustomers)
+	http.HandleFunc("/customers-xml", getAllCustomersXml)
+
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// to mimic a connection to a DB
+func generateCustomers() []Customer {
+	return []Customer{
+		{
+			Name:    "Steve",
+			City:    "Los Angeles",
+			Zipcode: "91505",
+		},
+		{
+			Name:    "Roland",
+			City:    "Boarderlands",
+			Zipcode: "00000",
+		},
+		{
+			Name:    "Firehawk",
+			City:    "badlands",
+			Zipcode: "00001",
+		},
+	}
+
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := generateCustomers()
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+}
+
+func getAllCustomersXml(w http.ResponseWriter, r *http.Request) {
+	customers := generateCustomers()
+
+	w.Header().Add("Content-Type", "application/xml")
+	xml.NewEncoder(w).Encode(customers)
+}
