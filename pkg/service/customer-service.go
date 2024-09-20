a32e0bd68558b83e818b97e8b6977c28de64eff7
Jason Stoddard <jason.stoddard@crowdstrike.com>
2024-09-20 09:36:48 -0400

    added service implementation

---
+++ pkg/service/customer-service.go
@@ -0,0 +1,19 @@
+package service
+
+import "github.com/modern-dev-dude/microservices-in-go/pkg/domain"
+
+type CustomerService interface {
+	GetAllCustomers() ([]domain.Customer, error)
+}
+
+type DefaultCustomerService struct {
+	repo domain.CustomerRepository
+}
+
+func (s DefaultCustomerService) GetAllCustomers() ([]domain.Customer, error) {
+	return s.repo.FindAll()
+}
+
+func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
+	return DefaultCustomerService{repo: repository}
+}
