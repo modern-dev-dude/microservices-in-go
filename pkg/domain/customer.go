713c285b6dc0238e7ceb857dc4485ed5adc61e2f
Jason Stoddard <jason.stoddard@crowdstrike.com>
2024-09-25 11:05:39 -0400

    added dto for domain to service communication

--- pkg/domain/customer.go
+++ pkg/domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/modern-dev-dude/microservices-in-go/pkg/errs"
+import (
+	"github.com/modern-dev-dude/microservices-in-go/pkg/dto"
+	"github.com/modern-dev-dude/microservices-in-go/pkg/errs"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -14,4 +17,24 @@ type Customer struct {
 type CustomerRepository interface {
 	FindAll(string) ([]Customer, *errs.AppErr)
 	GetCustomerById(string) (*Customer, *errs.AppErr)
+}
+
+func (c Customer) getStatusAsText() string {
+	statusTxt := "inactive"
+	if c.Status == "1" {
+		statusTxt = "active"
+	}
+
+	return statusTxt
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.getStatusAsText(),
+	}
 }
--- pkg/service/customer-service.go
+++ pkg/service/customer-service.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"github.com/modern-dev-dude/microservices-in-go/pkg/domain"
+	"github.com/modern-dev-dude/microservices-in-go/pkg/dto"
 	"github.com/modern-dev-dude/microservices-in-go/pkg/errs"
 )
 
 type CustomerService interface {
 	GetAllCustomers(string) ([]domain.Customer, *errs.AppErr)
-	GetCustomer(string) (*domain.Customer, *errs.AppErr)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppErr)
 }
 
 type DefaultCustomerService struct {
@@ -18,8 +19,14 @@ func (s DefaultCustomerService) GetAllCustomers(status string) ([]domain.Custome
 	return s.repo.FindAll(status)
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppErr) {
-	return s.repo.GetCustomerById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppErr) {
+	c, err := s.repo.GetCustomerById(id)
+	if err != nil {
+		return nil, err
+	}
+	response := c.ToDto()
+
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
