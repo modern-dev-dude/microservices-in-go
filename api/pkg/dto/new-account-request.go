8630a47e1b8674a58d0e314b358f22f41c1e9be7
Jason Stoddard <jason.stoddard@crowdstrike.com>
2024-10-16 10:47:42 -0400

    made refactor of new account type dto

--- api/pkg/dto/new-account-request.go
+++ api/pkg/dto/new-account-request.go
@@ -10,12 +10,23 @@ type NewAccountRequest struct {
 	Amount      float64 `json:"amount"`
 }
 
+const CHECKING = "checking"
+const SAVING = "checking"
+
+func (req NewAccountRequest) IsChecking() bool {
+	return req.AccountType == CHECKING
+}
+
+func (req NewAccountRequest) IsSavings() bool {
+	return req.AccountType == SAVING
+}
+
 func (req NewAccountRequest) Validate() *errs.AppErr {
 	if req.Amount < 5000 {
 		return errs.NewValidationError("you must have more than 5,000 to open a new account")
 	}
 
-	if req.AccountType != "saving" && req.AccountType != "checking" {
+	if !req.IsSavings() && !req.IsChecking() {
 		return errs.NewValidationError("account type must be checking or saving")
 	}
 
